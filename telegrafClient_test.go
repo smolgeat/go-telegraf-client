d46e99a6fba3819286d8efa7a5e3b8a322bac48d
Vin <29700078+smolgeat@users.noreply.github.com>
2022-03-06 20:50:06 -0500

    test dialing of UDP Server

--- telegrafClient_test.go
+++ telegrafClient_test.go
@@ -1,9 +1,81 @@
 package telegrafClient
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+)
 
-func TestBlank(t *testing.T) {
-	if 1 != 1 {
-		t.Error()
+func TestDialUDP(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	serverAddr, err := UDPServer(ctx, "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
 	}
+	defer cancel()
+
+	client, err := net.Dial("udp", serverAddr.String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = client.Close() }()
+
 }
+
+func UDPServer(ctx context.Context, addr string) (net.Addr, error) {
+	server, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("udp server failed %s %w", addr, err)
+	}
+	go func() {
+		go func() {
+			<-ctx.Done()
+			_ = server.Close
+
+		}()
+
+		buf := make([]byte, 1024)
+
+		for {
+			n, clientAddr, err := server.ReadFrom(buf)
+			if err != nil {
+				return
+
+			}
+
+			_, err = server.WriteTo(buf[:n], clientAddr)
+			if err != nil {
+				return
+			}
+		}
+	}()
+
+	return server.LocalAddr(), nil
+}
+
+// func TestWrite(t *testing.T) {
+
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	serverAddr, err := UDPServer(ctx, "127.0.0.1:")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer cancel()
+
+// 	var UDPClient client
+// 	var metrics metric
+
+// 	UDPClient.tags = make(map[string]string)
+// 	UDPClient.tags["my_tag_1"] = "foo"
+// 	UDPClient.tags["my_tag_2"] = "bar"
+// 	UDPClient.server = serverAddr.String()
+// 	metrics.measurement = make(map[string]string)
+// 	metrics.measurement["a"] = "5"
+// 	metrics.measurement["b"] = "6"
+// 	expected := []byte(`{"a":"5","b":"6","my_tag_1":"foo","my_tag_2":"bar"}`)
+// 	if UDPClient.Write(metrics) != string(expected) {
+// 		t.Fatal()
+// 	}
+
+// }
