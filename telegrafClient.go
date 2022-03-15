371f8ab4373fa01670b27296d7da6cdc44380e6b
Vin <29700078+smolgeat@users.noreply.github.com>
2022-03-14 23:53:01 -0500

    added skeleton to write metrics via TCP

--- telegrafClient.go
+++ telegrafClient.go
@@ -1,6 +1,11 @@
+/*Package telegrafClient sends metric to telegraf socketlistener over udp in JSON
+
+
+
+ */
+
 package telegrafClient
 
-//Package telegrafClient sends metric to telegraf socketlistener over udp in JSON
 import (
 	"bytes"
 	"encoding/json"
@@ -9,21 +14,41 @@ import (
 )
 
 type Client struct {
-	Server   string
-	Tags     map[string]string
-	Protocol string
+	Server   string            //Server IP and port
+	Tags     map[string]string //Global Tags
+	Protocol string            //Server Protocol
 }
 
 type Metric struct {
-	Measurement map[string]string
-	Tags        map[string]string
+	Measurement map[string]string // Metric Values
+	Tags        map[string]string // Metric Tags
 }
 
+//Write sends metrics to telegraf,
+//will use global Tags if metric Tags arent specified
 func (c Client) Write(metrics Metric) []byte {
-	// Writes metrics to telegraf will use global Tags if metric Tags arent specified
-	//
-	//
-	//
+
+	var result []byte
+
+	if c.Protocol == "UDP" {
+
+		result = c.WriteUDP(metrics)
+
+	} else if c.Protocol == "TCP" {
+
+		result = c.WriteTCP(metrics)
+
+	} else {
+		fmt.Printf("No protocol specified")
+	}
+
+	return result
+
+}
+
+//WriteUDP creates UDP client then sends metrics as bytes to UDP server
+func (c Client) WriteUDP(metrics Metric) []byte {
+
 	message := metrics.Measurement
 	serverAddr, err := net.ResolveUDPAddr("udp", c.Server)
 	if err != nil {
@@ -50,5 +75,14 @@ func (c Client) Write(metrics Metric) []byte {
 			fmt.Printf("Error: %s\n", err)
 		}
 	}
+	return msgInBytes.Bytes()
+}
+
+func (c Client) WriteTCP(metrics Metric) []byte {
+
+	placeholder := make(map[string]string)
+	msgInBytes := new(bytes.Buffer)
+	json.NewEncoder(msgInBytes).Encode(placeholder)
+
 	return msgInBytes.Bytes()
 }
